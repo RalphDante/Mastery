1c197da8bd7ebce3ff870cdb4865aa1352bb8aa4
Ralph Comandante <ralph.comandante1@gmail.com>
2025-01-10 19:24:23 +0800

    finally finished the add image feature for mobile phones

--- src/components/CreatePage/DisplayFlashCards.jsx
+++ src/components/CreatePage/DisplayFlashCards.jsx
@@ -22,9 +22,24 @@ function DisplayFlashCards({flashCards, setFlashCards, onDelete, autoResize}){
     const textareaRefs = useRef([])
 
 
-    // const onDrop = useCallback(acceptedFiles => {
-    //     console.log(acceptedFiles.name)
-    //   }, [])
+    const ImageUploader = ({ onImageUpload, index, field }) => {
+        const { getRootProps, getInputProps } = useDropzone({
+            onDrop: (acceptedFiles) => {
+                onImageUpload(acceptedFiles[0], field, index);
+            }
+        });
+    
+        return (
+            <div className='bg-blend-darken'> 
+ 
+            <i  {...getRootProps()} className="fa-sharp fa-regular fa-image text-2xl cursor-pointer hover:text-green-500"></i>
+                <input {...getInputProps()} />
+     
+                
+            </div>
+        );
+    };
+    
 
     const {getRootProps, getInputProps, isDragActive} = useDropzone({
         onDrop: (acceptedFiles) => {
@@ -155,30 +170,28 @@ function DisplayFlashCards({flashCards, setFlashCards, onDelete, autoResize}){
                                 {flashCard.question.startsWith('https://firebasestorage.googleapis.com') ? (
                                     <img src={flashCard.question} alt="Question" className={styles.flashCardImage} />
                                 ) : (
-                                    <textarea className={`${styles.flashCardInput} ${draggingOverStates[`${index}-question`]? styles.dragOver : ''}`} value={flashCard.question}
-                                    ref={el => textareaRefs.current[index * 2] = el}
-                                    onDragOver={(e)=>handleDragOver(e, 'question', index)}
-                                    onDrop={(e)=>handleDrop(e, 'question', index)}
-                                    onDragLeave={(e)=>handleDragLeave(e, 'question', index)}
-                                    onChange={(e)=>{
-                                        onEdit(index, 'question', e.target.value)
-                                        autoResize(e)
-                                    }}
-                                    placeholder='Question'
-                                    onInput={autoResize}
-                                    ></textarea>
+                                    <>
+                                        <textarea className={`${styles.flashCardInput} ${draggingOverStates[`${index}-question`]? styles.dragOver : ''}`} value={flashCard.question}
+                                            ref={el => textareaRefs.current[index * 2] = el}
+                                            onDragOver={(e)=>handleDragOver(e, 'question', index)}
+                                            onDrop={(e)=>handleDrop(e, 'question', index)}
+                                            onDragLeave={(e)=>handleDragLeave(e, 'question', index)}
+                                            onChange={(e)=>{
+                                                onEdit(index, 'question', e.target.value)
+                                                autoResize(e)
+                                            }}
+                                            placeholder='Question'
+                                            onInput={autoResize}
+                                            ></textarea>
+                                            <ImageUploader 
+                                                onImageUpload={handleImageClick}
+                                                index={index}
+                                                field="question"
+                                            /> 
+                                    </>
+                                    
                                     )
                                 }
-                              
-                                    <div {...getRootProps()}> 
-                                        <i  onClick={()=>(setChosenIndex(index),setChosenField('question'))} class="fa-sharp fa-regular fa-image text-2xl cursor-pointer hover:text-green-500" ></i>
-                                        <input {...getInputProps() } />
-                                    </div>
-
-                       
-
-
-
                             </div>
                             
                             <div className={`${styles.answerContainer} md:w-1/2 w-full`}>
@@ -188,37 +201,43 @@ function DisplayFlashCards({flashCards, setFlashCards, onDelete, autoResize}){
                                 {flashCard.answer.startsWith('https://firebasestorage.googleapis.com') ? (
                                     <img src={flashCard.answer} alt='Answer' className={styles.flashCardImage} />
                                 ) : (
-                                    <textarea className={`${styles.flashCardInput} ${draggingOverStates[`${index}-answer`] ? styles.dragOver : ''}`} value={flashCard.answer}
-                                    ref={el => textareaRefs.current[index * 2 + 1] = el}
-                                    onDragOver={(e)=>handleDragOver(e, 'answer', index)}
-                                    onDrop={(e)=>handleDrop(e, 'answer', index)}
-                                    onDragLeave={(e)=>handleDragLeave(e, 'answer', index)}
+                                    <>
+                                        <textarea className={`${styles.flashCardInput} ${draggingOverStates[`${index}-answer`] ? styles.dragOver : ''}`} value={flashCard.answer}
+                                        ref={el => textareaRefs.current[index * 2 + 1] = el}
+                                        onDragOver={(e)=>handleDragOver(e, 'answer', index)}
+                                        onDrop={(e)=>handleDrop(e, 'answer', index)}
+                                        onDragLeave={(e)=>handleDragLeave(e, 'answer', index)}
 
-                                    onChange={(e)=>{
-                                        onEdit(index, 'answer', e.target.value)
-                                        autoResize(e)
-                                    }}
-                                    placeholder='Answer'
-                                    onInput={autoResize}
-            
-                                    ></textarea>
+                                        onChange={(e)=>{
+                                            onEdit(index, 'answer', e.target.value)
+                                            autoResize(e)
+                                        }}
+                                        placeholder='Answer'
+                                        onInput={autoResize}
+                
+                                        ></textarea>
+                                        <ImageUploader 
+                                            onImageUpload={handleImageClick}
+                                            index={index}
+                                            field="answer"
+                                        /> 
+                                    
+                                    </>
+                                    
                                     
 
                                 )}
+                                
                                     
-                                        <div {...getRootProps()}> 
-                                            <i  onClick={()=>(setChosenIndex(index),setChosenField('answer'))} class="fa-sharp fa-regular fa-image text-2xl cursor-pointer hover:text-green-500" ></i>
-                                            <input {...getInputProps() } />
-                                        </div>
                                         
                                 
                             </div>
 
                                 
                         </div>
-                        <div>
+                        <div className=''>
                             <button className={`${styles.btn} ml-auto`} onClick={()=>onDelete(index)}>Delete</button>
-                            
+    
 
                         </div>
 
