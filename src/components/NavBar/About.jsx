64380462ae73a0a6575d542c8799224820e153ff
Ralph Comandante <ralph.comandante1@gmail.com>
2025-06-18 19:07:29 +0800

    removed my name

--- src/components/NavBar/About.jsx
+++ src/components/NavBar/About.jsx
@@ -3,7 +3,6 @@ function About(){
     return(
         <div>
         <h1>You are in the About Page</h1>
-        <h2>This website is a flash card study site made by yours truly Ralph</h2>
         <h2>Here's how to use this site</h2>
         <h2>You create a folder, then you create a file, then you create flash cards, and then you quiz yourself hehehe</h2>
         </div>
