a9e386393f8062a8900f53b8e0fbc3f4328d567a
RalphDante <ralph.comandante1@gmail.com>
2025-08-17 23:38:11 +0800

    fix: smart review tutorial glitch

--- src/pages/FlashCardsPage/FlashCardUI.jsx
+++ src/pages/FlashCardsPage/FlashCardUI.jsx
@@ -1449,7 +1449,7 @@ function FlashCardUI({
         </TutorialOverlay>
 
 
-        <TutorialOverlay isVisible={authUser && (goToDashBoard || isFinished || completedFirstReviewSession)}>
+        <TutorialOverlay isVisible={authUser && completedFirstReviewSession && isFinished}>
             <div className="relative space-y-4 bg-gray-800 rounded-2xl shadow-2xl border border-gray-700 max-w-lg w-full p-8">
             <div className="text-5xl mb-4">🎉</div>
             <h2 className="text-2xl font-bold bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text text-transparent">
